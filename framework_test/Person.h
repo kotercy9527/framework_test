2ce24426d9e8ea607cb2ca855e84d8724b943b96
kotercy <31883359+kotercy9527@users.noreply.github.com>
2024-09-08 17:17:26 +0800

    add some a new class

---
+++ framework_test/Person.h
@@ -0,0 +1,18 @@
+//
+//  Person.h
+//  framework_test
+//
+//  Created by zhipeng jiang on 2024/9/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
++ (void)saySomething;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ framework_test/Person.m
@@ -0,0 +1,14 @@
+//
+//  Person.m
+//  framework_test
+//
+//  Created by zhipeng jiang on 2024/9/8.
+//
+
+#import "Person.h"
+
+@implementation Person
++ (void)saySomething {
+    NSLog(@"a log from person class");
+}
+@end
